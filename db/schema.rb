a07a45872cfb517d664e9e652a159f9dff0a109a
James Gray <gray.f.james@gmail.com>
2021-04-20 22:47:32 +0000

    RSpec Changes

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_235755) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.text "desctiption"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
