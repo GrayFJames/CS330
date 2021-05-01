ce445eaebeb1b221a4481a4793a129414f3e5292
James Gray <gray.f.james@gmail.com>
2021-05-01 19:13:15 +0000

    Changed where the sign up link is

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-#RSpec.feature "Visiting the homepage", type: :feature do
-#  scenario "The visitor should see signup/signin links" do
-#    visit root_path
-#    expect(page).to have_text("Sign up")
-#    expect(page).to have_text("Sign in")
-#  end
-#end
+RSpec.feature "Visiting the homepage", type: :feature do
+  scenario "The visitor should see signup/signin links" do
+    visit root_path
+    expect(page).to have_text("Sign in")
+  end
+end
