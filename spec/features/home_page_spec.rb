d5273b56d2e1f0e72d19e3cf2c953655b30b0756
James Gray <gray.f.james@gmail.com>
2021-05-05 20:41:01 +0000

    Adding Bootstrap

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,5 +4,7 @@ RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see signup/signin links" do
     visit root_path
     expect(page).to have_text("Sign in")
+    expect(page).to have_text("Sign up")
+    expect(page).to have_text("Projects")
   end
 end
