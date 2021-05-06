ab97c00b8675c590fdbbaed2011b1a1e9668d677
James Gray <gray.f.james@gmail.com>
2021-05-06 01:34:11 +0000

    Bootstrap stuff

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "projects/index", type: :view do
 
   it "renders a list of projects" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "Title".to_s, :count => 0
+    assert_select "tr>td", :text => "MyText".to_s, :count => 0
   end
 end
